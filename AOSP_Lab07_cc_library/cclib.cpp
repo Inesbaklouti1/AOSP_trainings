64ea4427a9bb43555df46bbdcdb97664b9aa5ba0
Inesbaklouti1 <bakloutiines70@gmail.com>
2024-05-16 10:53:27 +0100

    modified

--- AOSP_Lab07_cc_library/cclib.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-int main(){
-	std::cout << "hi i am the cc library" << std::endl;
-	return 0;
-	}  
---
+++ AOSP_Lab07_cc_library/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "sum.h"
+int main(){
+	int result = sum(1,2);
+	std::cout << "the sum is:"<< result << std::endl;
+	return 0; 
+	}
